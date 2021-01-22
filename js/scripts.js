49e83da724aec61b80d4f540c746c6753a9d7518
Matt Foley <mattfoley0375@gmail.com>
2021-01-22 15:09:51 -0800

    thought I had to rewrite my code because of a potential typo in the instructions, very frustrated

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
 ///Business Logic Top
+function beepBoop(number) {
+  let answer = [];
+  for (i = 0; i <= number; i++) {
+    if (i.toString().includes(3)) {
+      answer.push("Won't you be my neigh-bot?");
+    } else if 
+  }
+
+
+///User Interface Logic
+$(document).ready(function() {
+  $("#InputNumber").submit(function(event) {
+    event.preventDefault();
+
