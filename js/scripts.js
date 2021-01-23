1f8eb822498aad738d82ec07f282317893bc04c5
Matt Foley <mattfoley0375@gmail.com>
2021-01-22 16:56:22 -0800

    final push this was hard but i think i made it harder than it should have been

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,41 @@
 
+
+
 ///User Interface Logic
 $(document).ready(function () {
-  $("#InputNumber").submit(function (event) {
+  $("form#form").submit(function (event) {
     event.preventDefault();
-    
-    const number = ($)
+    const number = ($("input#numVal").val());
+    const inputResult = arrayInput(number);
+    $("#result").text(inputResult);
 
-
-    $(".result").show();
-    $("#RobogerSays").text(combinedArray);
-    $("#RobogerSays").show();
-  }
-
+  });
+});
 
 
 
 ///Business Logic Top
 
+function arrayInput(numbers) {
+  let inputNumber = [];
+  for (let index = 0; index <= numbers; index += 1) {
+    const numbInput = index.toString();
 
+    const beep = "Beep!"
+    const boop = "Boop!"
+    const neighbot = "Won't you be my neigh-bot?"
+    
+    if (numbInput.includes(3)) {
+      inputNumber.push(neighbot);
+    } else if (numbInput.includes(2)) {
+      inputNumber.push(boop);
+    } else if (numbInput.includes(1)) {
+      inputNumber.push(beep);
+    } else {
+      inputNumber.push(numbInput);
+
+    }
+    }
+    return inputNumber;
+
+  }
