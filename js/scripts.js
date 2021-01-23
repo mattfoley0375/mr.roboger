09eccc6980d1f2acf41a5e54ee2ded08d32763df
Matt Foley <mattfoley0375@gmail.com>
2021-01-22 16:17:22 -0800

    changing my loop technique and ui because it was broken before

--- js/scripts.js
+++ js/scripts.js
@@ -5,18 +5,29 @@ function beepBoop(number) {
     if (i.toString().includes(3)) {
       answer.push("Won't you be my neigh-bot?");
     } else if (i.toString().includes(2)) {
-      answer.push("Boop!)
+      answer.push("Boop!")
     } else if (i.toString().includes(1)) {
       answer.push("Beep!")
     } else {
       answer.push(i);
     }
     return answer;
-  }
+  };
+
+}
+
 
 
 ///User Interface Logic
-$(document).ready(function() {
-  $("#InputNumber").submit(function(event) {
+$(document).ready(function () {
+  $("#InputNumber").submit(function (event) {
     event.preventDefault();
+    let numberInput = $("#numVal").val();
+    let ArrayTwo = beepBoop(numberInput);
+    let combinedArray = [];
+    combinedArray.push(ArrayTwo.join(" "));
 
+    $(".result").show();
+    $("#RobogerSays").text(combinedArray);
+    $("#RobogerSays").show();
+  }
