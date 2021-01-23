6f918afe5ee0fcbcc8a1d34b1417f886f362794b
Matt Foley <mattfoley0375@gmail.com>
2021-01-22 17:31:30 -0800

    fixed bug when zero is entered

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,22 @@
 ///Business Logic Top
 
-function arrayInput(numbers) {
+function arrayInput(number) {
   let inputNumber = [];
-  for (let i = 0; i <= numbers; i += 1) {
+  for (let i = 0; i <= number; i += 1) {
     const numberInput = i.toString();
-
-
     const boop = "Boop!"
     const neighbot = "Won't you be my neigh-bot?"
     const beep = "Beep!"
     const none = "Enter a number bigger than 0 silly human!"
     
-    if (numberInput.includes(3)) {
+    if (numberInput.includes(0)) {
+      inputNumber.push(none);
+    } else if (numberInput.includes(3)) {
       inputNumber.push(neighbot);
     } else if (numberInput.includes(2)) {
       inputNumber.push(boop);
     } else if (numberInput.includes(1)) {
       inputNumber.push(beep);
-    } else if (numberInput.includes(0)) {
-      inputNumber.push(none)
     } else {
       inputNumber.push(numberInput);
 
