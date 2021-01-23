e31fc6edfd1744cfa5fc93423b67091f1a648206
Matt Foley <mattfoley0375@gmail.com>
2021-01-22 16:19:21 -0800

    backtracking to my first approach

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,20 @@
-///Business Logic Top
-function beepBoop(number) {
-  let answer = [];
-  for (i = 0; i <= number; i++) {
-    if (i.toString().includes(3)) {
-      answer.push("Won't you be my neigh-bot?");
-    } else if (i.toString().includes(2)) {
-      answer.push("Boop!")
-    } else if (i.toString().includes(1)) {
-      answer.push("Beep!")
-    } else {
-      answer.push(i);
-    }
-    return answer;
-  };
-
-}
-
-
 
 ///User Interface Logic
 $(document).ready(function () {
   $("#InputNumber").submit(function (event) {
     event.preventDefault();
-    let numberInput = $("#numVal").val();
-    let ArrayTwo = beepBoop(numberInput);
-    let combinedArray = [];
-    combinedArray.push(ArrayTwo.join(" "));
+    
+    const number = ($)
+
 
     $(".result").show();
     $("#RobogerSays").text(combinedArray);
     $("#RobogerSays").show();
   }
+
+
+
+
+///Business Logic Top
+
+
